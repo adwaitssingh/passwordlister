f8ee03ea3e8e424e655aa186022e8227d602f1a6
Heduxw <114377235+adwaitssingh@users.noreply.github.com>
2023-09-10 11:16:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-for x in range (1,95)
-  
+for x in range (1,10):
+  for x in range (1,10):
+    for x in range (1,10):
+      for x in range (1,10):
+        for x in range (1,10):
+          for x in range (1,10):
+            for x in range (1,10):
+              for x in range (1,10):
+                for x in range (1,10):
